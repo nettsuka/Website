c5906f6d32bd9aa6c82e0b554616d7b61136ffdf
Nettsuka <jonathanchhou@Outlook.com>
2021-08-12 17:54:29 -0700

    add footer and descriptions

--- js/carousel.js
+++ js/carousel.js
@@ -1,14 +1,15 @@
-var carousel = function(dest, elem, rate){
+var carousel = function(dest, elem, rate, href){
   this.dest = dest;
   this.elem = elem;
   this.rate = parseInt(rate, 10) || 2000;
+  this.href = href;
   this.loopNum = 0;
   this.auto();
 };
 
 carousel.prototype.auto = function() {
   var i = this.loopNum++ % this.elem.length;
-  this.dest.innerHTML = '<img src="' +this.elem[i]+'">';
+  this.dest.innerHTML = '<a href= '+ this.href[i] +'><img src="' +this.elem[i]+'"><a/>';
   var that = this;
   setTimeout(function() {
     that.auto();
@@ -19,7 +20,8 @@ window.onload = function() {
   var element = document.getElementsByClassName('carousel')[0];
   var elem = element.getAttribute('data-type');
   var rate = element.getAttribute('data-rate');
-  new carousel(element, JSON.parse(elem), rate);
+  var href = element.getAttribute('href-type');
+  new carousel(element, JSON.parse(elem), rate, JSON.parse(href));
 }
 
 /*
